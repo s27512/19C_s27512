4b59374c7f90666a8115f3a2fda0f4918ef8d7c2
s27512 <s27512@pjwstk.edu.pl>
2024-03-21 16:26:35 +0100

    Task9 done.

--- main.py
+++ main.py
@@ -215,6 +215,70 @@ class SquareGenerator:
         print(cube_roots_1_to_10)
 
 
+    #TASK9
+
+    import math
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise ValueError("End of range must be greater than or equal to start.")
+
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+
+        if end < start:
+            raise ValueError("End of range must be greater than or equal to start.")
+
+        cubes = [x**3 for x in range(start, end + 1)]
+        return cubes
+
+    def calculate_cube_roots(self, numbers):
+
+        cube_roots = [math.pow(num, 1/3) for num in numbers]
+        return cube_roots
+
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise ValueError("End of range must be greater than or equal to start.")
+
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+
+    from square_package.square_generator import CubicGenerator
+
+    cubic_gen = CubicGenerator()
+
+    cubes_1_to_10 = cubic_gen.generate_cubes(1, 10)
+
+    cube_roots_1_to_10 = cubic_gen.calculate_cube_roots(cubes_1_to_10)
+
+    print("List of cubes from 1 to 10:")
+    print(cubes_1_to_10)
+
+    print("\nCube roots of numbers in the list:")
+    print(cube_roots_1_to_10)
+
+    try:
+        squares_10_to_1 = cubic_gen.generate_squares(10, 1)
+        print("\nList of squares from 10 to 1:")
+        print(squares_10_to_1)
+    except ValueError as e:
+        print("\nError:", e)
+
+
+
+
 
 
 
