8b576ae6b71e5a7e33199c9a3c86fe516c5fd92e
s27512 <s27512@pjwstk.edu.pl>
2024-03-21 15:34:30 +0100

    Task6 done.

--- main.py
+++ main.py
@@ -116,6 +116,26 @@ else:
 
 
 
+#TASK6
+
+import math
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+
+        if end < start:
+            raise ValueError("End of range must be greater than or equal to start.")
+
+        squares = [x**2 for x in range(start, end + 1)]
+        return squares
+
+    def calculate_square_roots(self, numbers):
+
+        square_roots = [math.sqrt(num) for num in numbers]
+        return square_roots
+
+
+
 
 
 
